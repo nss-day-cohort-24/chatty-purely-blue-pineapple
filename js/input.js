c2d70d03708789b1c63f7cdc64146ab593b9c7b2
marcdledda <marcdl636@gmail.com>
2018-02-08 11:26:27 -0600

    progress

---
+++ js/input.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("input.js reporting test");
--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
 "use strict";
 console.log("Hello test");
+
+let input = require("./input");
+
+
+
+// ===TESTING GROUNDS
+let dataRequest = new XMLHttpRequest();
+dataRequest.addEventListener("load", dataRequestLoad);
+dataRequest.addEventListener("error", dataRequestError);
+
+function dataRequestLoad(){
+    console.log("Loading...");
+    let messageData = JSON.parse(event.target.responseText);
+    console.log("messageData", messageData);
+    showMessage(messageData);
+}
+
+function showMessage(messageData){
+    let messageDiv = document.getElementById("messages");
+}
+
+function dataRequestError(){
+    console.log("Error Loading");
+}
+
+dataRequest.open("GET", "js/messages.json");
+dataRequest.send();
+
