2d67fe2d3a1aab4429e33f9bbe09354b18c84ad8
marcdledda <marcdl636@gmail.com>
2018-02-08 14:14:39 -0600

    MOAR PROGRESSSSSSSSS

--- js/input.js
+++ js/input.js
@@ -1,2 +1,59 @@
 "use strict";
 console.log("input.js reporting test");
+
+// ===PRELOADED MESSAGES
+let dataRequest = new XMLHttpRequest();
+dataRequest.addEventListener("load", dataRequestLoad);
+dataRequest.addEventListener("error", dataRequestError);
+
+function dataRequestLoad(event){
+    console.log("Loading...");
+    let messageData = JSON.parse(event.target.responseText);
+    console.log("messageData", messageData);
+    console.log(messageData[0].message);
+    showMessage(messageData);
+}
+
+function showMessage(inputJSON){
+    let messageDiv = document.getElementById("messages");
+    let messageStuff = '';
+    for (let item in inputJSON){
+        let messageItem = inputJSON[item];
+        console.log(inputJSON[item]);
+        messageStuff += "<div id=newMessage>" + messageItem.message + "<button id=delete type=`button`>X</button></div>";
+    }
+    messageDiv.innerHTML = messageStuff;
+}
+
+function dataRequestError(event){
+    console.log("Error Loading");
+}
+
+dataRequest.open("GET", "js/messages.json");
+dataRequest.send();
+
+module.exports = dataRequest;
+
+
+// ===INPUTTING MESSAGES
+let inputMessage = document.getElementById("navbar-user-message");
+inputMessage.addEventListener("keyup", messageEnter);
+let messageHistory = [];
+console.log("History:", messageHistory);
+
+function messageEnter(){
+    if (event.which === 13 || event.keyCode == 13) {
+        messageDisplay();
+        return false;
+    }
+}
+
+function messageDisplay(){
+    let messageDiv = document.getElementById("messages");
+    messageDiv.innerHTML += "<div id=newMssage>" + inputMessage.value + "<button tpye =`button`>X</button></div>";
+    messageHistory.unshift(inputMessage.value);
+    inputMessage.value = '';
+    console.log("New History:", messageHistory);
+}
+
+module.exports = {inputMessage, messageHistory};
--- js/main.js
+++ js/main.js
@@ -2,37 +2,3 @@
 console.log("Hello test");
 
 let input = require("./input");
-
-
-
-// ===TESTING GROUNDS
-let dataRequest = new XMLHttpRequest();
-dataRequest.addEventListener("load", dataRequestLoad);
-dataRequest.addEventListener("error", dataRequestError);
-
-function dataRequestLoad(event){
-    console.log("Loading...");
-    let messageData = JSON.parse(event.target.responseText);
-    console.log("messageData", messageData);
-    console.log(messageData[0].message);
-    showMessage(messageData);
-}
-
-function showMessage(inputJSON){
-    let messageDiv = document.getElementById("messages");
-    let messageStuff = '';
-    for (let item in inputJSON){
-        let messageItem = inputJSON[item];
-        console.log(inputJSON[item]);
-        messageStuff += "<div>" + messageItem.message + "</div>";
-    }
-    messageDiv.innerHTML = messageStuff;
-}
-
-function dataRequestError(event){
-    console.log("Error Loading");
-}
-
-dataRequest.open("GET", "js/messages.json");
-dataRequest.send();
-
