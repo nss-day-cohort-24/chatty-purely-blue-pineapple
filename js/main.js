924d0ff8c4311f591040c10da6264c8b2655ba25
marcdledda <marcdl636@gmail.com>
2018-02-08 12:32:32 -0600

    more PROGRESS

--- js/main.js
+++ js/main.js
@@ -10,18 +10,26 @@ let dataRequest = new XMLHttpRequest();
 dataRequest.addEventListener("load", dataRequestLoad);
 dataRequest.addEventListener("error", dataRequestError);
 
-function dataRequestLoad(){
+function dataRequestLoad(event){
     console.log("Loading...");
     let messageData = JSON.parse(event.target.responseText);
     console.log("messageData", messageData);
+    console.log(messageData[0].message);
     showMessage(messageData);
 }
 
-function showMessage(messageData){
+function showMessage(inputJSON){
     let messageDiv = document.getElementById("messages");
+    let messageStuff = '';
+    for (let item in inputJSON){
+        let messageItem = inputJSON[item];
+        console.log(inputJSON[item]);
+        messageStuff += "<div>" + messageItem.message + "</div>";
+    }
+    messageDiv.innerHTML = messageStuff;
 }
 
-function dataRequestError(){
+function dataRequestError(event){
     console.log("Error Loading");
 }
 
