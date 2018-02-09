58ece5ce32e015180587f2bf47afab32c12103e0
Meg Scholl <meghan.e.scholl@gmail.com>
2018-02-09 10:54:34 -0600

    disabled button will enable when new message is inputted

--- js/clear.js
+++ js/clear.js
@@ -6,11 +6,16 @@ let clearButton = document.getElementById("clear-btn").addEventListener("click",
     // console.log("clearButton function works");
     document.getElementById("messages").innerHTML=""; 
 
+//IF THERE ARE NO MESSAGES ON BOARD, DISABLE BUTTON
     if(messages.value > 0) {
         document.getElementById("clear-btn").disabled = false;
     } else{
-        document.getElementById("clear-btn").disabled = true;
+            document.getElementById("navbar-user-message").addEventListener("click", function enable(messages) {
+            document.getElementById("clear-btn").disabled = false; 
+        });
+            document.getElementById("clear-btn").disabled = true;
     } 
-})
+});
+
 
 module.exports = (clearButton);
